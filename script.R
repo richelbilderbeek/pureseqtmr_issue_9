3704a7372d05cdcbc7b2a25dbc20bf63f234961b
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-06 12:55:21 +0200

    More output and checks

--- script.R
+++ script.R
@@ -1,3 +1,10 @@
 install.packages("remotes")
 remotes::install_github("richelbilderbeek/pureseqtmrinstall", dependencies = TRUE)
+
+message("Installing PureseqTM")
 pureseqtmrinstall::install_pureseqtm()
+
+message("Check PureseqTM installation")
+pureseqtmr::check_pureseqtm_installation()
+
+message("All worked :-)")
